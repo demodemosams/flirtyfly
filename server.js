d72bf02cb3330bedc7248cdf75c422f5a5e2fdcf
demodemosams <demo.demo.sams@gmail.com>
2026-05-20 10:25:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -969,11 +969,12 @@ app.get(
 
 );
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
 
   console.log(
-    "Server running on port 3000"
+    `Server Running On ${PORT}`
   );
 
 });
-
