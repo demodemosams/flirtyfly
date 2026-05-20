4fd67fd170c1af6830efdac0f54f4700e2929da9
demodemosams <demo.demo.sams@gmail.com>
2026-05-20 15:18:30 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-
-  "mongodb+srv://demodemosams_db_user:RJcc1o6jRFXH6W82@cluster0.amaamuw.mongodb.net/flirtyfly?retryWrites=true&w=majority&appName=Cluster0"
-
+  "mongodb+srv://demodemosams_db_user:RJcc1o6jRFXH6W82@cluster0.amaamuw.mongodb.net/flirtyfly?retryWrites=true&w=majority"
 )
 
 .then(() => {
@@ -14,6 +12,6 @@ mongoose.connect(
 
 .catch((error) => {
 
-  console.log(error);
+  console.log("MongoDB Error:", error);
 
 });
