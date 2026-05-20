b21ca9f1745f70cadf12dba80f7d4c891cd79703
demodemosams <demo.demo.sams@gmail.com>
2026-05-20 10:22:48 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/flirtyfly")
+mongoose.connect(
+
+  "mongodb+srv://demodemosams_db_user:RJcc1o6jRFXH6W82@cluster0.amaamuw.mongodb.net/flirtyfly?retryWrites=true&w=majority&appName=Cluster0"
+
+)
 
 .then(() => {
 
